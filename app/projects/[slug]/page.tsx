864d102515b82adf231b6558acbff91f9e7691c5
longkeng <longnt2cvn@gmail.com>
2025-12-31 11:37:38 +0700

    format code

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -3,23 +3,21 @@ import { notFound } from 'next/navigation'
 import { MDXLayoutRenderer } from 'pliny/mdx-components'
 
 type PageProps = {
-  params: {
+  params: Promise<{
     slug: string
-  }
+  }>
 }
 
 export default async function ProjectPage({ params }: PageProps) {
-  const {slug} = await params
-  const project = allProjects.find(
-    (p) => p.slug === slug
-  )
+  const { slug } = await params
+  const project = allProjects.find((p) => p.slug === slug)
 
   if (!project) {
     notFound()
   }
 
   return (
-    <article className="prose mx-auto py-8 dark:prose-invert">
+    <article className="prose dark:prose-invert mx-auto py-8">
       <h1>{project.title}</h1>
       <p>{project.description}</p>
       <MDXLayoutRenderer code={project.body.code} />
